6f6583c6c8d4fd6dc74f613b2f3c2297e3e30ccb
wilbrone <wilbroneokoth@gmail.com>
2020-06-05 15:50:22 +0300

    add image collection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'click-2-shop', component: HomeComponent},
+
+  {path: '', pathMatch: 'full', redirectTo: 'click-2-shop'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
